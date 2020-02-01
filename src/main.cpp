f3b20322546a677f2970e702d3ec4ea667d7cc21
Ivica Duspara <ivicadus@gmail.com>
2020-02-01 20:18:03 +0100

    - Added 3 examples which were tested. Examples are working as intended.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 #include "grid.h"
 int main () {
-    auto gg = read_from_file("../../examples/sudoku.txt");
+    auto gg = read_from_file("../../examples/sudoku4.txt");
     std::cout << gg << '\n';
     gg.solve();
     std::cout << gg << '\n';
